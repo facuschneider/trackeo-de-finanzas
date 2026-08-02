162fa6a4744b6762526d03ca80f47725c8a91a01
facuschneider <schneiderfacu24@gmail.com>
2026-08-02 12:01:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+/* Configuración global de Supabase */
 window.ENV = {
-    SUPABASE_URL: "__SUPABASE_URL__",
-    SUPABASE_ANON_KEY: "__SUPABASE_ANON_KEY__"
+    SUPABASE_URL: 'https://uboabckurhvfopibbbeo.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVib2FiY2t1cmh2Zm9waWJiYmVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxOTcyNjgsImV4cCI6MjEwMDc3MzI2OH0.SoC89tyqBmGqDspVfy5SE7655fDUAt7edaoM7TKWoj8'
 };
